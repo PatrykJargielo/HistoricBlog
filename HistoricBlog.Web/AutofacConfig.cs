bde9050e2499f93c278438e72d8792be344931cc
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2017-01-12 08:27:44 +0100

    AutofacConfig and Autofac installed

---
+++ HistoricBlog.Web/AutofacConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+
+namespace HistoricBlog.Web
+{
+    public class AutofacConfig
+    {
+        public static void SetUpAutofac()
+        {
+
+            var asembly = AppDomain.CurrentDomain.GetAssemblies();
+
+            var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyTypes(asembly).Where(t => t.Name.EndsWith("Service")).AsImplementedInterfaces();
+            builder.RegisterAssemblyTypes(asembly).Where(t => t.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerRequest();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // Register dependencies in filter attributes
+            builder.RegisterFilterProvider();
+
+            // Register dependencies in custom views
+            builder.RegisterSource(new ViewRegistrationSource());
+
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
