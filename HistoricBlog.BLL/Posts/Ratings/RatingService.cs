284ed11d115e32f2662022a0980691d1769317f8
Patryk Jargieło <jargielo.patryk@gmail.com>
2017-01-17 11:18:53 +0100

    category and rating services

---
+++ HistoricBlog.BLL/Posts/Ratings/RatingService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HistoricBlog.BLL.Base;
+using HistoricBlog.DAL.Base;
+using HistoricBlog.DAL.Posts.Ratings;
+
+namespace HistoricBlog.BLL.Posts.Ratings
+{
+    class RatingService : GenericService<Rating>, IRatingService
+    {
+        private IGenericRepository<Rating> _ratingRepository;
+
+        public RatingService(IGenericRepository<Rating> ratingRepository) : base(ratingRepository)
+        {
+            _ratingRepository = ratingRepository;
+        }
+
+        public override GenericResult<Rating> Update(Rating entity)
+        {
+            var result = new GenericResult<Rating>();
+            return Update(entity, result);
+
+        }
+
+        public override GenericResult<Rating> Delete(Rating entity)
+        {
+            var result = new GenericResult<Rating>();
+            return Delete(entity, result);
+        }
+
+        public override GenericResult<Rating> Create(Rating entity)
+        {
+            var result = new GenericResult<Rating>();
+
+            return Create(entity, result);
+        }
+    }
+}
