1ebbe4510fafab5dad16a774b2acc8a0241e1758
Patryk Jargieło <jargielo.patryk@gmail.com>
2017-01-20 10:40:05 +0100

    Update Models

--- HistoricBlog.WebApi/Models/Users/UserViewModel.cs
+++ HistoricBlog.WebApi/Models/Users/UserViewModel.cs
@@ -9,8 +9,5 @@ namespace HistoricBlog.WebApi.Models.Users
         public string Surname { get; set; }
         public string Login { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
-        public virtual IList<CommentViewModel> Comments { get; set; }
-      
     }
 }
