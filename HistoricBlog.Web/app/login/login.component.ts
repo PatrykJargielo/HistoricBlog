b3478ec60e944b648f5e50c9f0b14c1b22534d0a
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2017-02-02 09:28:23 +0100

    login module

--- HistoricBlog.Web/app/login/login.component.ts
+++ HistoricBlog.Web/app/login/login.component.ts
@@ -1,8 +1,14 @@
-﻿import {Component} from '@angular/core';
+﻿import { Component } from '@angular/core';
+import { ILogin } from "./login";
+import {LoginService} from "./login.service";
 
 @Component({
     templateUrl:'app/login/login.component.html'
 })
 export class LoginComponent {
-    
+    login: ILogin
+
+    constructor(private loginService: LoginService) {
+        
+    }
 }
---
+++ HistoricBlog.Web/app/login/login.module.ts
@@ -0,0 +1,17 @@
+﻿import { NgModule } from '@angular/core';
+import { LoginComponent } from "./login.component";
+import {LoginService} from "./login.service";
+
+@NgModule({
+    declarations: [
+        LoginComponent
+    ],
+    imports: [],
+    providers: [
+        LoginService
+    ]
+    
+})
+export class LoginModule {
+    
+}
